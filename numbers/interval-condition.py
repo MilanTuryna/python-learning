3dd61c0d5dd148fd556e40751bd13b930b775525
Miloslav Turyna <miloslavturyna@seznam.cz>
2023-01-03 11:33:48 +0100

    Create interval-condition.py

---
+++ numbers/interval-condition.py
@@ -0,0 +1,11 @@
+# Jsou dány hodnoty A, B a pole kladných celých čísel. Zjistěte, kolik čísel je z intervalu <A,B> (větší, rovno A a zároveň menší, rovno B).
+
+a = 10
+b = 50
+array = [45,57,854,147,84,15,584,12,15,5647]
+count = 0
+for x in array:
+    if(x >= a and =< b):
+        count += 1
+print("Počet čísel splňujicí podmínku:", count)
+
